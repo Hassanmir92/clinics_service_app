ba9d2b18cecf4635829acc6a5a17b48a1da9d2a0
Hassan Mir <hassanmir92@gmail.com>
2017-05-12 22:03:23 +0100

    [Poscode Endpoint] Getting data from data.gov
    * Requiring “Request” package to make http request
    * Getting postcode data back

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express');
+var request = require("request");
 
 var app = express();
 
-app.get('/', function (req, res) {
-  res.status(200).json({ "response": "ready" });
-});
+app.get('/clinics/postcode/:postcode', function (req, res) {
+  var response = request("https://data.gov.uk/data/api/service/health/clinics/partial_postcode?partial_postcode=" + req.params.postcode, function(error, response, body) {
+    var body = JSON.parse(body);
 
+    res.status(response.statusCode).json({ "response": body })
+  });
+});
 
 app.listen(3000, function () {
   console.log('App listening on port 3000');
