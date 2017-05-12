a8d61d1dbf45de5ab8278fb9395369389e4d4bc9
Hassan Mir <hassanmir92@gmail.com>
2017-05-12 23:13:59 +0100

    [Postcode Endpoint] filtering resource result
    * Taking the first partial of the postcode before the space
    * Calling the clinic postcode resource using the partial postcode
    * Filter out results matching the full postcode and return with wanted attributes.

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 var express = require('express');
-var request = require("request");
+var request = require('request');
 
 var app = express();
 
 app.get('/clinics/postcode/:postcode', function (req, res) {
-  var response = request("https://data.gov.uk/data/api/service/health/clinics/partial_postcode?partial_postcode=" + req.params.postcode, function(error, response, body) {
+  // Taking the first partial of postcode before the space
+  var partial_postcode = req.params.postcode.split(" ")[0];
+
+  // Calling the clinic postcode resource using the partial postcode
+  request("https://data.gov.uk/data/api/service/health/clinics/partial_postcode?partial_postcode=" + partial_postcode, function(error, response, body) {
     var body = JSON.parse(body);
 
-    res.status(response.statusCode).json({ "response": body })
+    // Filter out results matching the full postcode and return with wanted attributes
+    var results = body.result.filter(function (clinic) {
+      return clinic.postcode == req.params.postcode;
+    }).map(function (clinic) {
+      return {
+        organisation_id: clinic.organisation_id,
+        name: clinic.name
+      };
+    });
+
+    res.status(response.statusCode).json({ "results": results })
   });
 });
 
